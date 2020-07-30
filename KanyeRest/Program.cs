7183f1b139522068b52f76fe83e5f83035924f0c
MBurnette4120 <mikeburnette@rocketmail.com>
2020-07-30 14:36:33 -0400

    Add project files.

---
+++ KanyeRest/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeRest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+        }
+    }
+}
